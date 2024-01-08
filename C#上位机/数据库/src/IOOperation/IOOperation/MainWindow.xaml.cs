eba78976fa7509138ba6e7c102273ea85bcb38ea
BqFang <1571658067@qq.com>
2024-01-09 00:47:21 +0800

    IO文件操作WPF前端页面初步

---
+++ C#上位机/数据库/src/IOOperation/IOOperation/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IOOperation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //// 1.创建文件流
+            //FileStream fs = new FileStream("E:\\上位机\\C#上位机\\数据库\\src\\myfile.txt", FileMode.Create);
+            //// 2.创建写入器
+            //StreamWriter sw = new StreamWriter(fs);
+            //// 3.以流的方式进行写入
+            //sw.Write(this)
+        }
+
+        
+    }
+}
